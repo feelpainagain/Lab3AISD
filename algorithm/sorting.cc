896ae0d0d8b00ca32f4704ff4077c834c6ad445a
feelpainagain <132294998+feelpainagain@users.noreply.github.com>
2023-12-27 01:05:20 +0400

    realized all 3 sorts + tests + write to file WIP

--- algorithm/sorting.cc
+++ algorithm/sorting.cc
@@ -1,9 +1,22 @@
 #include <iostream>
 #include <random>
+#include <vector>
+#include <fstream>
 
 using namespace std;
 
 namespace algorithm {
+
+	vector<int> random_seed(int a, int b, int n, int seed) {
+		vector<int> res;
+		mt19937 generator(seed); //генерит рандомные числа
+		uniform_int_distribution<> distribution(a, b); //генерирует случайное значение в диапазоне
+		for (int i = 0; i < n; i++) {
+			int x = distribution(generator);
+			res.push_back(x);
+		}
+		return res;
+	}
 
 	struct stats {
 		size_t comparison_count = 0;
@@ -17,31 +30,44 @@ namespace algorithm {
 			comparison_count = other.comparison_count;
 			copy_count = other.copy_count;
 		}
+		stats& operator+=(const stats& other) {
+			comparison_count += other.comparison_count;
+			copy_count += other.copy_count;
+			return *this;
+		}
+		stats& operator/(const int other) {
+			comparison_count /= other;
+			copy_count /= other;
+			return *this;
+		}
 	};
+	vector<int> ordered_vector(int n) {
+		vector<int> vector;
+		for (int i = 1; i <= n; i++) {
+			vector.push_back(i);
+		}
+		return vector;
+	}
 
-	void swap(int& a, int& b, stats& stats) {
-		int temp = a;
+	vector<int> reversed_ordered_vector(int n) {
+		vector<int> vector;
+		for (int i = n; i > 0; i--) {
+			vector.push_back(i);
+		}
+		return vector;
+	}
+
+	template<typename T>
+	void swap(T& a, T& b, stats& stats) {
+		T temp = a;
 		a = b;
 		b = temp;
 		stats.copy_count += 3;
 	}
 
 	template<typename T>
-	stats insert_sort(vector<T>& arr) {
-		stats cur_stat;
-		for (int i = 1; i < arr.size(); i++) {
-			cur_stat.comparison_count++;
-			for (int j = i; j > 0 && arr[j - 1] > arr[j]; j--) {
-				cur_stat.copy_count++;
-				std::swap(arr[j - 1], arr[j]);
-			}
-		}
-		return cur_stat;
-	}
-
-	}
-	int partition(std::vector<int>& arr, int low, int high, stats& stats) {
-		int pivot = arr[high];
+	int partition(vector<T>& arr, int low, int high, stats& stats) { //деление на 2 массива с пивотом
+		T pivot = arr[high];
 		int i = (low - 1);
 		for (int j = low; j <= high - 1; j++) {
 			if (arr[j] <= pivot) {
@@ -54,19 +80,111 @@ namespace algorithm {
 		return (i + 1);
 	}
 
-	stats quickSort(std::vector<int>& arr, int low, int high) {
+	template<typename T>
+	stats quick_sort(vector<T>& arr, int low, int high) { //быстрая сортировка
 		stats stat;
 		if (low < high) {
 			int pi = partition(arr, low, high, stat);
-			stats left_stats = quickSort(arr, low, pi - 1);
-			stats right_stats = quickSort(arr, pi + 1, high);
-			stat.comparison_count+= left_stats.comparison_count + right_stats.comparison_count;
+			stats left_stats = quick_sort(arr, low, pi - 1);
+			stats right_stats = quick_sort(arr, pi + 1, high);
+			stat.comparison_count += left_stats.comparison_count + right_stats.comparison_count;
 			stat.copy_count += left_stats.copy_count + right_stats.copy_count;
 		}
 		return stat;
 	}
+
 	template<typename T>
-	std::ostream& operator << (std::ostream& os, const std::vector<T> vec) {
+	void pyramid(vector<T>& arr, int n, int i, stats& stats) { //преобразование в бинарное дерево
+		int largest = i;
+		int left = 2 * i + 1;
+		int right = 2 * i + 2;
+		if (left < n && ++stats.comparison_count && arr[left] > arr[largest])
+			largest = left;
+		if (right < n && ++stats.comparison_count && arr[right] > arr[largest])
+			largest = right;
+		if (largest != i) {
+			swap(arr[i], arr[largest], stats);
+			++stats.copy_count;
+			pyramid(arr, n, largest, stats);
+		}
+	}
+
+	template<typename T>
+	stats pyramid_sort(vector<T>& arr) { //пирамидальная
+		stats stat;
+		stat.comparison_count = 0;
+		stat.copy_count = 0;
+		int n = arr.size();
+		for (int i = n / 2 - 1; i >= 0; --i)
+			pyramid(arr, n, i, stat);
+		for (int i = n - 1; i >= 0; --i) {
+			swap(arr[0], arr[i], stat);
+			++stat.copy_count;
+			pyramid(arr, i, 0, stat);
+		}
+		return stat;
+	}
+
+
+
+	template<typename T>
+	stats insert_sort(vector<T>& arr) { //вставками
+		stats cur_stat;
+		for (int i = 1; i < arr.size(); i++) {
+			cur_stat.comparison_count++;
+			for (int j = i; j > 0 && arr[j - 1] > arr[j]; j--) {
+				cur_stat.copy_count++;
+				std::swap(arr[j - 1], arr[j]);
+			}
+		}
+		return cur_stat;
+	}
+
+stats create_random_vector(int length_vector, int trial_count, int sort) {
+		stats stat;
+		for (int i = 0; i < trial_count; i++) {
+			std::vector<int> test = random_seed(-1000, 1000, length_vector, i);
+			cout << "iteration: " << i << endl;
+			switch (sort) {
+			case 1:
+				stat += insert_sort(test);
+				break;
+			case 2:
+				stat += quick_sort(test, 0, test.size() - 1);
+				break;
+			case 3:
+				stat += pyramid_sort(test);
+				break;
+			}
+		}
+		stat = stat / trial_count;
+		return stat;
+	}
+
+	vector<stats> create_hundred_random_vectors(int sort_choice) {
+		vector<stats> stat;
+		for (int i = 1; i < 11; i++) {
+			stat.push_back(create_random_vector(i * 1000, 100, sort_choice));
+			cout << "len: " << i * 1000 << endl;
+		}
+		cout << "len: " << 25000 << endl;
+		stat.push_back(create_random_vector(25000, 100, sort_choice));
+		cout << "len: " << 50000 << endl;
+		stat.push_back(create_random_vector(50000, 100, sort_choice));
+		cout << "len: " << 100000 << endl;
+		stat.push_back(create_random_vector(100000, 100, sort_choice));
+		return stat;
+	}
+	void write_to_file(vector<stats> stat) {
+		ofstream out;
+		out.open("C:/Users/Alex/Desktop/AaDS/lab_3/random_bubble.txt");
+		for (int i = 0; i < stat.size(); i++) {
+			out << stat[i].comparison_count << " " << stat[i].copy_count << endl;
+		}
+		out.close();
+	}
+	template<typename T>
+	ostream& operator << (ostream& os, const vector<T> vec) {
 		os << "{ ";
 		for (int i = 0; i < vec.size(); i++) {
 			if (i < vec.size() - 1)
@@ -77,7 +195,13 @@ namespace algorithm {
 		}
 		return os;
 	}
-	std::ostream& operator <<(std::ostream& os, const stats description) {
+	bool operator<(const string& lhs, const string& rhs) {
+		return lhs.compare(rhs) < 0;
+	}
+	bool operator>(const string& lhs, const string& rhs) {
+		return lhs.compare(rhs) > 0;
+	}
+	ostream& operator <<(ostream& os, const stats description) {
 		os << "Compasion count: " << description.comparison_count << "; " << "Copy count: " << description.copy_count << endl;
 		return os;
 	}
